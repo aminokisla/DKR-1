69141fd43785cec73a4521b11778e50d5c15a923
Амина <151569260+aminokisla@users.noreply.github.com>
2023-11-20 23:28:49 +0300

    Add files via upload

---
+++ ex1.pas
@@ -0,0 +1,18 @@
+﻿program ex1;
+var x,y,z: real;
+begin
+  write ('x= ');
+  read(x);
+  if x<-8 then 
+    y:=-x
+  else 
+    if x<-3 then
+      y:=((power(x,2))/(log(x)-sin(x)))/3
+    else
+      if (x>=-3)and(x<6) then
+        y:=(exp(x))/(power(x,(0.1*x)))
+        else
+          if x>=6 then
+            y:=sin(x)*power(x,2);
+   writeln('y= ', y);
+end.
---
+++ ex2.pas
@@ -0,0 +1,17 @@
+﻿program ex2;
+var x: real;
+begin
+   x:=-10;
+while x<=8 do 
+  begin
+    if x<-8 then
+      writeln('y = ',-x:1:2) else
+      if x<-3 then 
+        writeln ('y = ',(((power(x,2))/(log(x)-sin(x)))/3):1:2) else 
+        if (x>=-3) and (x<6) then
+          writeln ('y = ', (exp(x))/(power(x,(0.1*x))):1:2) else
+          if x>=6 then 
+            writeln ('y = ',sin(x)*power(x,2):1:2); 
+              x:=x+0.2;
+  end;
+end.
